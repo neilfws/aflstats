82d007c1e034708049f28a66a565786ac68c4725
Neil Saunders <neilfws@gmail.com>
2015-06-02 08:20:17 +1000

    Latest team percentage data and output

--- code/R/aflblues.R
+++ code/R/aflblues.R
@@ -8,4 +8,4 @@ blues         <- blues[order(blues$date, decreasing = T),]
 
 blues.2007  <- subset(blues, date > as.Date("27-Jul-2007", "%d-%b-%Y"))
 blues.2007$Coach <- ifelse(blues.2007$Year > 2012, "Malthouse", "Ratten")
-ggplot(blues.2007) + geom_boxplot(aes(factor(Year), Percent, fill = Coach)) + theme_bw()
+ggplot(blues.2007) + geom_boxplot(aes(factor(Year), Percent, fill = Coach)) + theme_bw() + scale_fill_manual(values = c("cornflowerblue", "darkorange")) + labs(x = "Year", y = "Percentage", title = "Carlton game percentages by season under Ratten and Malthouse")
