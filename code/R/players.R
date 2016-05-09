31ff59ce6734b0d5f5070145be8d5bb657706cca
Neil Saunders <neilfws@gmail.com>
2016-05-09 22:25:33 +1000

    rewrite with functions

--- code/R/players.R
+++ code/R/players.R
@@ -9,32 +9,36 @@ getDOB <- function(u) {
   p <- htmlTreeParse(u, useInternalNodes = TRUE)
   d <- str_match(xpathSApply(p, "//body", xmlValue), "Born:(.*?) ")[,2]
   # sleep for multiple requests
-  Sys.sleep(3)
+  Sys.sleep(2)
   return(d)
 }
 
-players.2016 <- readHTMLTable("http://afltables.com/afl/stats/2016.html")
-players.2016 <- players.2016[2:19]
-doc <- htmlTreeParse("http://afltables.com/afl/stats/2016.html", useInternalNodes = TRUE)
-teams <- xpathSApply(doc, "//th/a", xmlValue)
-teams <- teams[seq(1, 36, 2)]
-for(i in 1:18) {
-    players.2016[[i]]$team <- teams[i]
+getAges <- function(year) {
+  players.y <- readHTMLTable(paste("http://afltables.com/afl/stats/", year, ".html", sep = ""))
+  players.y <- players.y[2:19]
+  doc <- htmlTreeParse(paste("http://afltables.com/afl/stats/", year, ".html", sep = ""), useInternalNodes = TRUE)
+  teams <- xpathSApply(doc, "//th/a", xmlValue)
+  teams <- teams[seq(1, 36, 2)]
+  for(i in 1:18) {
+      players.y[[i]]$team <- teams[i]
+  }
+  players <- xpathSApply(doc, "//a[@href]", xmlAttrs)
+  players <- ldply(players, rbind)
+  players <- players[grep("players", players$href), ]
+  baseurl <- "http://afltables.com/afl/stats"
+  players$target <- paste(baseurl, players$href, sep = "/")
+  players$dob <- sapply(players$target, function(x) getDOB(x))
+  players.y.df <- ldply(players.y, rbind)
+  players$age <- as.Date(Sys.Date(), "%Y-%m-%d") - as.Date(players$dob, "%e-%b-%Y")
+  players.y.df$age <- as.numeric(players$age)
+  return(players.y.df)
 }
-players <- xpathSApply(doc, "//a[@href]", xmlAttrs)
-players <- ldply(players, rbind)
-players <- players[grep("players", players$href), ]
-baseurl <- "http://afltables.com/afl/stats"
-players$target <- paste(baseurl, players$href, sep = "/")
-players$dob <- sapply(players$target, function(x) getDOB(x))
-players.2016.df <- ldply(players.2016, rbind)
-players$age <- as.Date(Sys.Date(), "%Y-%m-%d") - as.Date(players$dob, "%e-%b-%Y")
-players.2016.df$age <- as.numeric(players$age)
 
 #plot
-png(file = "output/players2016.png", width = 800, height = 760)
-ggplot(players.2016.df) + geom_boxplot(aes(team, age), fill = "salmon") + 
-  theme_bw() + ylab("Current players age (days)") + 
-  theme(axis.text.x = element_text(angle = 90)) + 
-  labs(title = ("Player age (in days) distribution by team"))
+players <- getAges(2016)
+png(file = "output/playerAges.png", width = 800, height = 760)
+ggplot(players) + geom_violin(aes(reorder(team, age, median), age/365), fill = "darkorange", linetype = 0) +
+  theme_bw() + ylab("Current players age (years)") +
+  theme(axis.text.x = element_text(angle = 90)) +
+  labs(title = ("Player age distribution by team 2016 sorted by median age"), x = "Team") + geom_hline(yintercept = median(players$age/365), linetype = "dashed") + stat_summary(aes(team, as.numeric(age) / 365), fun.y = "median", geom = "point")
 dev.off()
