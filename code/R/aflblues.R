87e3586462cdc92908c2418504fdd4ed914a9697
Neil Saunders <neilfws@gmail.com>
2017-03-28 17:26:53 +1100

    Updated strptime for dates + post-Malthouse coaches

--- code/R/aflblues.R
+++ code/R/aflblues.R
@@ -1,11 +1,13 @@
-source('~/Dropbox/projects/aflstats/code/R/afltables.R')
+source('~/Dropbox/projects/github_projects/aflstats/code/R/afltables.R')
 
 blues         <- allGames("carlton")
 blues$Year    <- allGamesYear(blues)
-blues$date    <- as.Date(blues$Date, "%d-%b-%Y %I:%M PM")
+blues$date    <- as.Date(blues$Date, "%a %d-%b-%Y %I:%M PM")
 blues$Percent <- allGamesPercent(blues)
 blues         <- blues[order(blues$date, decreasing = T),]
 
 blues.2007  <- subset(blues, date > as.Date("27-Jul-2007", "%d-%b-%Y"))
 blues.2007$Coach <- ifelse(blues.2007$Year > 2012, "Malthouse", "Ratten")
-ggplot(blues.2007) + geom_boxplot(aes(factor(Year), Percent, fill = Coach)) + theme_bw() + scale_fill_manual(values = c("cornflowerblue", "darkorange")) + labs(x = "Year", y = "Percentage", title = "Carlton game percentages by season under Ratten and Malthouse")
+blues.2007$Coach <- ifelse(blues.2007$Year == 2015, "Malthouse/Barker", blues.2007$Coach)
+blues.2007$Coach <- ifelse(blues.2007$Year > 2015, "Bolton", blues.2007$Coach)
+ggplot(blues.2007) + geom_boxplot(aes(factor(Year), Percent, fill = Coach)) + theme_bw() + scale_fill_brewer(palette = "Spectral") + labs(x = "Year", y = "Percentage", title = "Carlton game percentages by season under different coaches")
