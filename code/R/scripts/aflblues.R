aa1c67721656ad61ef0a89145ec10267f85d8562
Neil Saunders <neilfws@gmail.com>
2018-07-22 22:41:55 +1000

    Update to use tidyverse + fitzRoy

--- code/R/scripts/aflblues.R
+++ code/R/scripts/aflblues.R
@@ -1,13 +1,21 @@
-source('~/Dropbox/projects/github_projects/aflstats/code/R/scripts/afltables.R')
+library(tidyverse)
+library(fitzRoy)
 
-blues         <- allGames("carlton")
-blues$Year    <- allGamesYear(blues)
-blues$date    <- as.Date(blues$Date, "%a %d-%b-%Y %I:%M PM")
-blues$Percent <- allGamesPercent(blues)
-blues         <- blues[order(blues$date, decreasing = T),]
-
-blues.2007  <- subset(blues, date > as.Date("27-Jul-2007", "%d-%b-%Y"))
-blues.2007$Coach <- ifelse(blues.2007$Year > 2012, "Malthouse", "Ratten")
-blues.2007$Coach <- ifelse(blues.2007$Year == 2015, "Malthouse/Barker", blues.2007$Coach)
-blues.2007$Coach <- ifelse(blues.2007$Year > 2015, "Bolton", blues.2007$Coach)
-ggplot(blues.2007) + geom_boxplot(aes(factor(Year), Percent, fill = Coach)) + theme_bw() + scale_fill_brewer(palette = "Spectral") + labs(x = "Year", y = "Percentage", title = "Carlton game percentages by season under different coaches")
+match_results %>% 
+  filter(Home.Team == "Carlton" | Away.Team == "Carlton") %>% 
+  mutate(Percent = ifelse(Home.Team == "Carlton", 
+                          100 * (Home.Points/Away.Points), 
+                          100 * (Away.Points/Home.Points)),
+         Coach = ifelse(Season > 2012, "Malthouse", "Ratten"),
+         Coach = ifelse(Season == 2015, "Malthouse/Barker", Coach),
+         Coach = ifelse(Season > 2015, "Bolton", Coach)) %>% 
+  filter(Season > 2006, 
+         Round.Type == "Regular") %>% 
+  ggplot(aes(factor(Season), Percent)) + 
+    geom_boxplot(aes(fill = Coach))  + 
+    geom_hline(aes(yintercept = median(Percent))) + 
+    scale_fill_brewer(palette = "Spectral") + 
+    labs(x = "Year", 
+         y = "Percentage", 
+         title = "Carlton game percentages by season under different coaches") +
+  theme_bw()
